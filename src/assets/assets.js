b175eac68851258cb7fcbbc7d1a1c33d6978e1c0
Aryan Kumar Arya <abcunofficialkumar@gmail.com>
2025-06-16 22:23:50 +0530

    routes and pages set up complete

---
+++ src/assets/assets.js
@@ -0,0 +1,114 @@
+import img1a from './1a.jpg';
+import img1b from './1b.jpg';
+import img1c from './1c.jpg';
+import img1d from './1d.jpg';
+import img1e from './1e.jpg';
+
+
+import img2a from './2a.jpg';
+import img2b from './2b.jpg';
+import img2c from './2c.jpg';
+import img2d from './2d.jpg';
+import img2e from './2e.jpg';
+
+
+import img3a from './3a.jpg';
+import img3b from './3b.jpg';
+import img3c from './3c.jpg';
+import img3d from './3d.jpg';
+
+
+import img4a from './4a.jpg';
+import img4b from './4b.jpg';
+import img4c from './4c.jpg';
+import img4d from './4d.jpg';
+import img4e from './4e.jpg';
+
+
+import img5a from './5a.jpg';
+import img5b from './5b.jpg';
+import img5c from './5c.jpg';
+import img5d from './5d.jpg';
+import img5e from './5e.jpg';
+import img5f from './5f.jpg';
+import img5g from './5g.jpg';
+
+import img6a from './6a.jpg';
+import img6b from './6b.jpg';
+import img6c from './6c.jpg';
+import img6d from './6d.jpg';
+import img6e from './6e.jpg';
+import img6f from './6f.jpg';
+import img6g from './6g.jpg';
+
+import img7a from './7a.jpg';
+import img7b from './7b.jpg';
+import img7c from './7c.jpg';
+import img7d from './7d.jpg';
+import img7e from './7e.jpg';
+import img7f from './7f.jpg';
+
+
+import img8a from './8a.jpg';
+import img8b from './8b.jpg';
+import img8c from './8c.jpg';
+import img8d from './8d.jpg';
+import img8e from './8e.jpg';
+import img8f from './8f.jpg';
+
+
+import img9a from './9a.jpg';
+import img9b from './9b.jpg';
+import img9c from './9c.jpg';
+import img9d from './9d.jpg';
+import img9e from './9e.jpg';
+import img9f from './9f.jpg';
+import img9g from './9g.jpg';
+import mercedes from './mercedes.jpeg'
+
+export const assets = {
+  mercedes,
+}
+
+
+
+
+export const products = [
+  {
+    _id: "1a",
+    name: "Product 1a",
+    description: "A sample product description.",
+    price: 100,
+    image: [img1a],
+    category: "Sample Category",
+    subCategory: "Sample Subcategory",
+    sizes: ["S", "M", "L"],
+    date: 1716634345448,
+    bestseller: true
+  },
+  {
+    _id: "1b",
+    name: "Product 1b",
+    description: "A sample product description.",
+    price: 100,
+    image: [img1b],
+    category: "Sample Category",
+    subCategory: "Sample Subcategory",
+    sizes: ["S", "M", "L"],
+    date: 1716634345448,
+    bestseller: true
+  },
+  // ... and so on up to:
+  {
+    _id: "9g",
+    name: "Product 9g",
+    description: "A sample product description.",
+    price: 100,
+    image: [img9g],
+    category: "Sample Category",
+    subCategory: "Sample Subcategory",
+    sizes: ["S", "M", "L"],
+    date: 1716634345448,
+    bestseller: true
+  }
+];
